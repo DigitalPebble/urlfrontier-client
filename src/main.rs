eab7b897b81abe7e17b80d3399486556b80c8159
Julien Nioche <julien@digitalpebble.com>
2022-12-05 22:17:50 +0000

    use subcommand for GetStats
    Signed-off-by: Julien Nioche <julien@digitalpebble.com>

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,34 @@
-
 pub mod frontier {
     tonic::include_proto!("urlfrontier");
 }
 
+use clap::{Parser, Subcommand};
 use frontier::url_frontier_client::UrlFrontierClient;
-use frontier::{QueueWithinCrawlParams};
+use frontier::QueueWithinCrawlParams;
 use tonic::Request;
-use clap::Parser;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-   /// Host of the URLFrontie service to connect to
-   #[arg(long, default_value_t = String::from("localhost"))]
-   host: String,
+    /// Host of the URLFrontier service to connect to
+    #[arg(long, default_value_t = String::from("localhost"))]
+    host: String,
 
-   /// Port of the URLFrontier service to connect to
-   #[arg(short, long, default_value_t = 7071)]
-   port: usize,
+    /// Port of the URLFrontier service to connect to
+    #[arg(short, long, default_value_t = 7071)]
+    port: usize,
+
+    #[command(subcommand)]
+    action: Action,
 }
 
+#[derive(Subcommand, Debug)]
+enum Action {
+    GetStats,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-
     let args = Args::parse();
 
     let mut address = "http://".to_string();
@@ -31,10 +36,20 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     address.push_str(":");
     address.push_str(&args.port.to_string());
 
-   let mut client = UrlFrontierClient::connect(address).await?;
-   let response = client.get_stats(Request::new(QueueWithinCrawlParams{key: String::from(""), crawl_id: String::from(""), local: false}))
-   .await?;
+    let mut client = UrlFrontierClient::connect(address).await?;
 
-println!("RESPONSE = {:?}", response);
-     Ok(())
+    match args.action {
+        Action::GetStats => {
+            let response = client
+                .get_stats(Request::new(QueueWithinCrawlParams {
+                    key: String::from(""),
+                    crawl_id: String::from(""),
+                    local: false,
+                }))
+                .await?;
+            println!("{:?}", response);
+        }
+    }
+
+    Ok(())
 }
